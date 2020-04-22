b1b1be21f9b06bdb39bef8ef7742ebb636fef547
Gergely Brautigam <gergely@arangodb.com>
2020-04-22 16:21:12 +0200

    Added ability to parse out attachments

---
+++ plugin/confluence_attachment_block.go
@@ -0,0 +1,34 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+
+package plugin
+
+import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+	md "github.com/arangodb-managed/html-to-markdown"
+)
+
+// ConfluenceAttachments converts `<ri:attachment ri:filename="oasis.png" ri:version-at-save="1" />` elements
+func ConfluenceAttachments() md.Plugin {
+	return func(c *md.Converter) []md.Rule {
+		return []md.Rule{
+			md.Rule{
+				Filter: []string{"ri:attachment"},
+				Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+					if v, ok := selec.Attr("ri:filename"); ok {
+						formatted := fmt.Sprintf("![][%s]", v)
+						return md.String(formatted)
+					}
+					return md.String("")
+				},
+			},
+		}
+	}
+}
